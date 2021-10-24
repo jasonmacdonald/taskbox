5859f36c177c89fce48f0858cab2a6f47313ce2d
Jason MacDonald <5718527+jasonmacdonald@users.noreply.github.com>
2021-10-24 16:35:23 -0400

    Updates

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -4,7 +4,7 @@ import * as TaskStories from './Task.stories';
 
 export default {
     component: PureTaskList,
-    title: 'TaskList',
+    title: 'Components/Tasks/TaskList',
     decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>]
 };
 
